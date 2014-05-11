878dee4907331887a7903788ac7c1f41e1893aca
Victor <victor.dev@gmail.com>
2014-05-10 22:06:15 -0400

    added project

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        // GET: Product
+        public ViewResult List(string category, int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel();
+            model.Products = _repository.Products.Where(p=> category == null || p.Category == category).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize);
+            model.PagingInfo = new PagingInfo { 
+                CurrentPage = page, 
+                ItemsPerPage = PageSize, 
+                TotalItems = category == null ? _repository.Products.Count() : _repository.Products.Where(e => e.Category == category).Count() };
+            model.CurrentCategory = category;
+            return View(model);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            #region Mock object
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> { 
+                new Product { Name = "Football", Price = 25, Category = "Sport" },
+                new Product { Name = "Surf board", Price = 179, Category = "Sport" },
+                new Product { Name = "Running shoes", Price = 95, Category = "Sport" }
+            });
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            #endregion
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
